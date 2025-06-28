dd339a26748b9a78a6c3e601e66b3ee7f747d628
deepak <deepakjgrt99@gmail.com>
2025-06-28 09:37:54 +0530

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by DEEPAK BEHERA on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
